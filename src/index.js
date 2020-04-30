e9899f9a6390a0ace780186c64e71deba1e9471f
svetkinsky <tebsv@inbox.ru>
2020-04-30 18:29:00 +0300

    localstorage integrated

--- src/index.js
+++ src/index.js
@@ -8,10 +8,8 @@ Xhr.sendRequest('GET', requestUrl)
     .then(data => run(data))
     .catch(err => console.log(err))
 
-const body = {
-    name: 'LPU № 1',
-    phone: '123456789'
-}
+
+
 
 //функция запуска программы
 const run = responseData => {
@@ -23,37 +21,89 @@ const run = responseData => {
 
     let maxId = 0
 
-    //перебор массива для заполнения таблицы
-    content.forEach(lpu => {
-        if (lpu.id > maxId) {
-            maxId = lpu.id
-        }
-        tableBody.append(Lpu.create(lpu.id, lpu.name, lpu.address, lpu.phone))
+    localStorage.clear()
+    content.forEach(item => {
+        localStorage.setItem(`lpu${item.id}`, JSON.stringify(item))
     })
+
+
+
+    //перебор массива для заполнения таблицы
+    for(let key in localStorage) {
+        if(!localStorage.hasOwnProperty(key)) {
+            continue
+        }
+
+        // console.log(key)
+            const lpu = JSON.parse(localStorage.getItem(key))
+            if (lpu.id > maxId) {
+                maxId = lpu.id
+            }
+            tableBody.append(Lpu.create(lpu.id, lpu.name, lpu.address, lpu.phone))
+
+    }
+
+
+
+    //функция редактирования ячейки таблицы и "отправки запроса" на изменение
+    const editRow = rowEdit => {
+        const cellsEdit = rowEdit.getElementsByTagName('td')
+        Array.from(cellsEdit).forEach(cellEdit => {
+            cellEdit.addEventListener('dblclick', event => {
+                let textArea = document.createElement('textarea');
+                textArea.style.width = cellEdit.clientWidth + 'px';
+                textArea.style.height = cellEdit.clientHeight + 'px';
+                textArea.className = 'edit-area'
+
+                textArea.value = cellEdit.innerHTML;
+                cellEdit.innerHTML = '';
+                cellEdit.appendChild(textArea);
+                textArea.focus();
+
+                console.log('cell ', cellEdit)
+
+                textArea.addEventListener('blur', () => {
+                    cellEdit.innerHTML = textArea.value
+                    const key = cellEdit.getAttribute('data-type')
+                    const lpuEdit = cellEdit.parentElement
+
+                    console.log(lpuEdit)
+                    console.log(lpuEdit.getAttribute('lpu-id'))
+
+                    console.log(localStorage.getItem(`lpu${lpuEdit.getAttribute('lpu-id')}`))
+                    // console.log(key)
+                    // const body = {}
+                    // body
+                    // body[key] = cellEdit.innerHTML
+                    //
+                    // console.log(body)
+                    //"запрос" на изменение данных
+
+                })
+            })
+        })
+    }
 
     const addButton = document.querySelector('.add')
 
     //добавление нового элемента
     addButton.addEventListener('click', () => {
-        const newLpu = Lpu.create(maxId)
+        const newLpu = Lpu.create(++maxId)
         tableBody.append(newLpu)
+
+        editRow(newLpu)
     })
 
-    const allCells = document.querySelectorAll('td')
-    allCells.forEach(cell => {
-        cell.addEventListener('dblclick', event => {
-            console.log('dblclick')
-            let textArea = document.createElement('textarea');
-            textArea.style.width = cell.clientWidth + 'px';
-            textArea.style.height = cell.clientHeight + 'px';
-            textArea.className = 'edit-area'
+    const allRows = document.querySelectorAll('tr')
 
-            textArea.value = cell.innerHTML;
-            cell.innerHTML = '';
-            cell.appendChild(textArea);
-            textArea.focus();
-        })
+    //редактирование ячеек таблицы
+
+    allRows.forEach(row => {
+        editRow(row)
     })
+
+
+
 }
 
 
--- src/lpu.js
+++ src/lpu.js
@@ -5,11 +5,15 @@ export const Lpu = {
         const lpuAddressCell = document.createElement('td')
         const lpuPhoneCell = document.createElement('td')
 
+        lpuNameCell.setAttribute('data-type', 'name')
+        lpuAddressCell.setAttribute('data-type', 'address')
+        lpuPhoneCell.setAttribute('data-type', 'phone')
+
         lpuNameCell.innerHTML = name
         lpuAddressCell.innerHTML = address
         lpuPhoneCell.innerHTML = phone
 
-        lpuRow.setAttribute('lpu-id', 'id')
+        lpuRow.setAttribute('lpu-id', id)
 
         lpuRow.append(lpuNameCell)
         lpuRow.append(lpuAddressCell)
