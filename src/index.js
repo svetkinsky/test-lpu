9f305f55f4f06de18308e2f7ff9f30e83fcefbd9
svetkinsky <tebsv@inbox.ru>
2020-05-01 18:57:54 +0300

    changed interface yet

--- src/index.js
+++ src/index.js
@@ -12,143 +12,69 @@ Xhr.sendRequest('GET', requestUrl)
 
 //функция запуска программы
 const run = responseData => {
-    console.log(responseData)
-
     let maxId = 0
-    //массив данных по ЛПУ
+
+    //массив данных по ЛПУ из заглушки
     const content = responseData.content || []
     const tableBody = document.querySelector('#table-body')
 
-
-
     // localStorage.clear()
-    // content.forEach(item => {
-    //     localStorage.setItem(`lpu${item.id}`, JSON.stringify(item))
-    // })
+
+    //функция вывода данных из LocalStorage в таблицу
+    const addDataFromLS = () => {
+        for(let key in localStorage) {
+            if(!localStorage.hasOwnProperty(key)) {
+                continue
+            }
+            if(key.indexOf('lpu') === -1) {
+                continue
+            }
+
+            const lpu = JSON.parse(localStorage.getItem(key))
+
+            if (lpu.id > maxId) {
+                maxId = lpu.id
+            }
+            tableBody.append(Lpu.create(lpu.id, lpu.name, lpu.address, lpu.phone))
+
+        }
+
+    }
+
+
+    //Кнопка добавления заглушки в LocalStorage
+    const addDataButton = document.querySelector('.add-data-button')
+    addDataButton.addEventListener('click', () => {
+        console.log('PIU')
+        localStorage.clear()
+        content.forEach(item => {
+            localStorage.setItem(`lpu${item.id}`, JSON.stringify(item))
+        })
+        addDataFromLS()
+    })
 
 
 
     //перебор массива для заполнения таблицы
-    for(let key in localStorage) {
-        if(!localStorage.hasOwnProperty(key)) {
-            continue
-        }
-        if(key.indexOf('lpu') === -1) {
-            continue
-        }
-
-        const lpu = JSON.parse(localStorage.getItem(key))
-        console.log('lpu from localstorage', lpu)
-
-        if (lpu.id > maxId) {
-            maxId = lpu.id
-        }
-        tableBody.append(Lpu.create(lpu.id, lpu.name, lpu.address, lpu.phone))
-
-    }
+    addDataFromLS()
 
 
-
-    //функция редактирования ячейки таблицы и "отправки запроса" на изменение
-    const editRow = rowEdit => {
-        const cellsEdit = rowEdit.getElementsByTagName('td')
-        Array.from(cellsEdit).forEach(cellEdit => {
-            cellEdit.addEventListener('dblclick', event => {
-                let textArea = document.createElement('textarea');
-                textArea.style.width = cellEdit.clientWidth + 'px';
-                textArea.style.height = cellEdit.clientHeight + 'px';
-                textArea.className = 'edit-area'
-
-                textArea.value = cellEdit.innerHTML;
-                cellEdit.innerHTML = '';
-                cellEdit.appendChild(textArea);
-                textArea.focus();
-
-                console.log('cell ', cellEdit)
-
-                textArea.addEventListener('blur', () => {
-                    cellEdit.innerHTML = textArea.value
-                    const key = cellEdit.getAttribute('data-type')
-                    const lpuEdit = cellEdit.parentElement
-
-                    console.log(lpuEdit.firstChild.nextSibling.innerHTML)
-                    const body = {
-                        id: lpuEdit.getAttribute('lpu-id'),
-                        name: lpuEdit.firstChild.innerHTML,
-                        address: lpuEdit.firstChild.nextSibling.innerHTML,
-                        phone: lpuEdit.lastChild.innerHTML
-                    }
-
-
-
-                    console.log(localStorage.getItem(`lpu${lpuEdit.getAttribute('lpu-id')}`))
-                    //"запрос" на изменение данных
-                    localStorage.removeItem(`lpu${lpuEdit.getAttribute('lpu-id')}`)
-                    localStorage.setItem(`lpu${lpuEdit.getAttribute('lpu-id')}`, JSON.stringify(body))
-
-                })
-            })
-        })
-
-    }
-
-    const removeLpu = row => {
-        row.addEventListener('contextmenu', event => {
-            event.preventDefault()
-            const removeButton = document.createElement('a')
-            removeButton.classList.add('waves-effect', 'waves-light', 'btn')
-            removeButton.innerHTML = 'Удалить'
-
-            row.style.position = 'relative'
-            removeButton.style.position = 'absolute'
-            removeButton.style.left = event.layerX + 'px'
-            row.append(removeButton)
-
-            removeButton.addEventListener('click', () => {
-                console.log('CLICK')
-                localStorage.removeItem(`lpu${row.getAttribute('lpu-id')}`)
-                row.remove()
-                removeButton.style.display = 'none'
-            })
-
-            removeButton.addEventListener('mouseout', () => {
-                removeButton.style.display = 'none'
-            })
-
-        })
-    }
-
-    const addButton = document.querySelector('.add')
 
     //добавление нового элемента
+    const addButton = document.querySelector('.add')
     addButton.addEventListener('click', () => {
-        const newLpu = Lpu.create(maxId++)
+        const newLpu = Lpu.create(++maxId)
         tableBody.append(newLpu)
 
-        editRow(newLpu)
-        removeLpu(newLpu)
 
         const newLpuObject = {
-            id: maxId++,
+            id: maxId,
             name: '',
             address: '',
             phone: ''
         }
         localStorage.setItem(`lpu${newLpuObject.id}`, JSON.stringify(newLpuObject))
     })
-
-    const allRows = document.querySelectorAll('tr')
-
-    //редактирование ячеек таблицы
-
-    allRows.forEach(row => {
-        editRow(row)
-        removeLpu(row)
-    })
-
-
-
-
 
 }
 
--- src/lpu.js
+++ src/lpu.js
@@ -19,7 +19,81 @@ export const Lpu = {
         lpuRow.append(lpuAddressCell)
         lpuRow.append(lpuPhoneCell)
 
+        Lpu.removeLpu(lpuRow)
+        Lpu.editRow(lpuRow)
+
         return lpuRow
+    },
+
+    //функция удаления строки таблицы и "отправки запроса" на удаление
+    removeLpu(row) {
+        row.addEventListener('contextmenu', event => {
+            event.preventDefault()
+            const removeButton = document.createElement('a')
+            removeButton.classList.add('waves-effect', 'waves-light', 'btn')
+            removeButton.innerHTML = 'Удалить'
+
+            row.style.position = 'relative'
+            removeButton.style.position = 'absolute'
+            removeButton.style.left = event.layerX + 'px'
+            row.append(removeButton)
+
+            removeButton.addEventListener('click', () => {
+                console.log('CLICK')
+                localStorage.removeItem(`lpu${row.getAttribute('lpu-id')}`)
+                row.remove()
+                removeButton.style.display = 'none'
+            })
+
+            removeButton.addEventListener('mouseout', () => {
+                removeButton.style.display = 'none'
+            })
+
+        })
+    },
+
+    //функция редактирования ячейки таблицы и "отправки запроса" на изменение
+    editRow(rowEdit) {
+        const cellsEdit = rowEdit.getElementsByTagName('td')
+        Array.from(cellsEdit).forEach(cellEdit => {
+            cellEdit.addEventListener('dblclick', event => {
+                let textArea = document.createElement('textarea');
+                textArea.style.width = cellEdit.clientWidth + 'px';
+                textArea.style.height = cellEdit.clientHeight + 'px';
+                textArea.className = 'edit-area'
+
+                textArea.value = cellEdit.innerHTML;
+                cellEdit.innerHTML = '';
+                cellEdit.appendChild(textArea);
+                textArea.focus();
+
+                console.log('cell ', cellEdit)
+
+                textArea.addEventListener('blur', () => {
+                    cellEdit.innerHTML = textArea.value
+                    const key = cellEdit.getAttribute('data-type')
+                    const lpuEdit = cellEdit.parentElement
+
+                    console.log(lpuEdit.firstChild.nextSibling.innerHTML)
+                    const body = {
+                        id: lpuEdit.getAttribute('lpu-id'),
+                        name: lpuEdit.firstChild.innerHTML,
+                        address: lpuEdit.firstChild.nextSibling.innerHTML,
+                        phone: lpuEdit.lastChild.innerHTML
+                    }
+
+
+
+                    console.log(localStorage.getItem(`lpu${lpuEdit.getAttribute('lpu-id')}`))
+                    //"запрос" на изменение данных
+                    localStorage.removeItem(`lpu${lpuEdit.getAttribute('lpu-id')}`)
+                    localStorage.setItem(`lpu${lpuEdit.getAttribute('lpu-id')}`, JSON.stringify(body))
+
+                })
+            })
+        })
+
     }
+
 }
 
